b6caa5b93c9b869cfe13a3dd4ed88c63a623141e
ROU S3NPAI <reyfredb@gmail.com>
2020-08-03 13:37:12 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProductsComponent } from './POS/products/products.component';
+import { PopularComponent } from './POS/popular/popular.component';
+import { FoodsComponent } from './POS/foods/foods.component';
+import { CoffeeComponent } from './POS/coffee/coffee.component';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'popular', component: PopularComponent },
+  { path: 'foods', component: FoodsComponent }, 
+  { path: 'coffee', component: CoffeeComponent },
+  { path: '', redirectTo:'/products' , pathMatch: 'full' }
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PopularComponent } from './POS/popular/popular.component';
+import { FoodsComponent } from './POS/foods/foods.component';
+import { CoffeeComponent } from './POS/coffee/coffee.component';
+import { ProductsComponent } from './POS/products/products.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopularComponent,
+    FoodsComponent,
+    CoffeeComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
